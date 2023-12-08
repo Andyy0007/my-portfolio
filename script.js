39345f21e9e15fbe335b08ae05b0ea8263aaf1b2
anadi mishra <98818583+Andyy0007@users.noreply.github.com>
2023-12-08 12:56:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// typing Animation
+
+var typed = new Typed(".typing", {
+    strings: ["Student", "Developer" , "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+var typed = new Typed(".typing-2", {
+    strings: ["Student", "Developer" , "Youtuber"],
+    typeSpeed: 98,
+    backSpeed: 58,
+    loop: true
+})
